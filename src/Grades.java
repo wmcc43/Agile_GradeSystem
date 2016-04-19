cdc5385c2de85dd029ddbefe26033c76687b4618
wmcc43 <wmcc43@gmail.com>
2016-04-19 20:25:02 +0800

    -add Grades header

--- src/Grades.java
+++ src/Grades.java
@@ -1,14 +1,24 @@
-
+/**
+ * A small class witch saves student name , student ID and grade data
+ * 
+ *
+ */
 public class Grades {
 	String ID;
 	String name;
 	int lab1, lab2, lab3;
 	int midTerm, finalExam, totalGrade;
-	
+	/**
+	 * Constructor
+	 */
 	public Grades(){
 		
 	}
-
+	/**
+	 * Calculate total grade by weights
+	 * @param Weights double array for string five grades
+	 * @return An integer contain calculated total grade
+	 */
 	public int calculateTotalGrade(double weights[]){
 		int weightGrade = (int)Math.round((((double)lab1)*weights[0]
 						+ ((double)lab2)*weights[1]
