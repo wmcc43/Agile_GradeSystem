e57da26f116e33d901ec2191000407744cd531f4
wmcc43 <wmcc43@gmail.com>
2016-04-19 14:29:51 +0800

    -add two customized exception's class header

--- src/NoSuchCommandExceptions.java
+++ src/NoSuchCommandExceptions.java
@@ -1,5 +1,13 @@
 
+/**
+ * If user type a instruction which not exist, then throw this Exception
+ */
 @SuppressWarnings("serial")
 public class NoSuchCommandExceptions extends Exception {
-
+	/**
+	 * Construct a NoSuchCommandException object.
+	 */
+	public NoSuchCommandExceptions() {
+		
+	}
 }
--- src/NoSuchIDExceptions.java
+++ src/NoSuchIDExceptions.java
@@ -1,5 +1,12 @@
 
+/**
+ * If cannot find someone's ID in the system, then throw this Exception
+ */
 @SuppressWarnings("serial")
 public class NoSuchIDExceptions extends Exception {
-
+	/**
+	 * Construct a NoSuchIDExceptions object.
+	 */
+	public NoSuchIDExceptions() {
+	}
 }
